8df51ac33e7ec3acffbbb0a3cad1e05e099cf363
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-07-18 22:17:28 +0900

    Creating an object inside the ref() function.
    When passing an object to the ref() function, you can render the data
    into the DOM the way I did before. There is a not optimal way:
    you can pass the whole object and render the data using dot notation,
    like user.name or user.age.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <section class="container">
-    <h2>{{ userName }}</h2>
+    <h2>{{ user.name }}</h2>
+    <h2>{{ user.age }}</h2>
+    <h2>{{ user }}</h2>
   </section>
 </template>
 
@@ -8,15 +10,28 @@
 import { ref } from 'vue';
 export default {
   setup () {
-    const uName = ref("Diego"); //better store the ref() in a vanilla JS variable. Don't use this keyword.
-                                //ref() creates an object.
+    // const uName = ref("Diego"); //better store the ref() in a vanilla JS variable. Don't use this keyword.
+    //                             //ref() creates an object.
+    // const uAge = ref(36);
+
+    const user = ref({
+      name: "Diego",
+      age: 36
+    });
+
+    console.log(user);
 
     setTimeout(function () {
-      uName.value = "Didi";
+      // uName.value = "Didi";
+      // uAge.value = 26;
+      user.value.name = 'Didi';
+      user.value.age = 26;
     }, 2000)
 
     return {
-      userName: uName
+      userName: user.value.name,
+      age: user.value.age,
+      user: user
     };
   } //the setup method replaces the Data, Methods, Watch and computed all together. The other are untouched.
   // data() {
