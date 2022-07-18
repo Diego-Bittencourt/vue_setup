2b996984a72f266d86b16a680c08f54a6d8dff73
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-07-18 16:46:11 +0900

    Initial commit - Vue Setup learning

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <section class="container">
+    <h2>{{ userName }}</h2>
+  </section>
+</template>
+
+<script>
+export default {
+  setup () {
+
+  } //the setup method replaces the Data, Methods, Watch and computed all together. The other are untouched.
+  // data() {
+  //   return {
+  //     userName: 'Maximilian',
+  //   };
+  // },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+.container {
+  margin: 3rem auto;
+  max-width: 30rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+  text-align: center;
+}
+</style>
