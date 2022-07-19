872ff366ddedb8bd3d72cc3c1140eaf7493d9c25
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-07-19 14:19:06 +0900

    Add computed properties
    Adding computed properties can be done by importing computed() from vue.
    You can then wrap an anonimous function that works like a computed.
    Keep in mind that computed() are ready only.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
   <section class="container">
-    <h2>{{ user.name }}</h2>
+    <h2>{{ userName }}</h2>
     <h2>{{ user.age }}</h2>
     <h2>{{ user }}</h2>
     <button @click="setAge">Change Age</button>
+    <div>
+      <input type="text" placeholder="first name" @input="setFirstName"/>
+      <input type="text" placeholder="last name" @input="setLastName"/>
+    </div>
   </section>
 </template>
 
 <script>
-import { reactive } from 'vue';
+import { reactive, ref, computed } from 'vue';
+
 // import { isReactive, isRef } from 'vue'; 
 // the isReactive and isRef are helpers to check is a value is reactive or not. it's a function that return true or false.
 
@@ -18,23 +23,41 @@ import { reactive } from 'vue';
 // using that, I can expose uName.value in return and they'll be reactive.
 export default {
   setup () {
+    const firstName = ref('');
+    const lastName = ref('');
+
+   const uName =  computed(function () {
+      return firstName.value + ' ' + lastName.value;
+    });
+
+
+  function setFirstName(event) {
+    firstName.value = event.target.value;
+  }
+
+  function setLastName(event) {
+    lastName.value = event.target.value
+  }
     // const uName = ref("Diego"); //better store the ref() in a vanilla JS variable. Don't use this keyword.
     //                             //ref() creates an object.
     // const uAge = ref(36);
 
     const user = reactive({
-      name: "Diego",
+      // name: "Diego",
       age: 36
     }); //reactive is specialized to work with objects. refs is for any data, including object.
 
       function setNewAge() {
         user.age++;
-      }
+      } // to expose functions, just create a function in the setup and expose it in the return.
 
 
       return {
       user: user,
-      setAge: setNewAge
+      setAge: setNewAge,
+      setFirstName,
+      setLastName,
+      userName: uName
     };// the ref() and reactive() objective are reactive, that means they change when data is changed.
       // in the other hand, the value inside them are not reactive. So it's important to return the object, no the
       //value inside them.
