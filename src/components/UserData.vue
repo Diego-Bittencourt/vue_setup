21a23f7206ba1ca20f42b24e630822ab2019ce75
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-07-20 15:28:44 +0900

    Using life cycles with composition API.
    The cycle hooks are replaced by functions that are imported from vue.
    Each function receives a function as argument that runs in that hook
    moment.

--- src/components/UserData.vue
+++ src/components/UserData.vue
@@ -10,10 +10,48 @@
 </template>
 
 <script>
-import { computed, inject } from 'vue';
+import {
+  computed,
+  inject,
+  onBeforeMount,
+  onBeforeUnmount,
+  onMounted,
+  onUnmounted,
+  onUpdated,
+  onBeforeUpdate,
+} from 'vue';
+
 export default {
   props: ['firstName', 'lastName'],
   setup(props, context) {
+
+
+    //The hook cycles are being replaced by these function that can be imported from vue. 
+    //Only the Created and BeforeCreated are replaced by the setup() itself.
+    //All the lyfe cycles receives a function that runs in that cycle.
+    onBeforeUpdate(function() {
+        console.log("Before Updated");
+    })
+onBeforeMount( function() {
+    console.log("On before mount")
+})
+  onBeforeUnmount(function() {
+    console.log("Before unmount")
+  })
+  onMounted(function() {
+    console.log("On Mounted")
+  })
+  onUnmounted(function() {
+    console.log("On Unmounted")
+  })
+  onUpdated(function() {
+    console.log("On Update")
+  })
+  onBeforeUpdate(function() {
+    console.log("Before Update")
+  })
+
+
     //the context has information about slot, if being used and emit events.
     // the emit event can be used with context.emit('event');
     const uName = computed(function () {
@@ -24,11 +62,10 @@ export default {
     //the inject function must be imported from vue and injected like this. The variable then must be exposed in the
     //return object. NEVER change the value in the injected end. Always change the value from the provided end.
 
-
     console.log(context);
     return {
       uName,
-      age
+      age,
     };
   },
 };
