d1826f8e2c47a1f6912a24561ca72e8c3a70c549
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-07-20 14:52:51 +0900

    using the context argument.
    the second argument in the setup() is the context.
    The context holds a series of functions like slots, props that fall
    and emit events.

--- src/components/UserData.vue
+++ src/components/UserData.vue
@@ -1,25 +1,27 @@
 <template>
-<h1>Start components</h1>
-<h2>First name:{{ firstName }}</h2>
-<h2>Last name: {{ lastName }}</h2>
-<h3>{{ age }}</h3>
-<h2>Full name: {{ uName }}</h2>
-<h1>End component</h1>
+  <div>
+    <h1>Start components</h1>
+    <h2>First name:{{ firstName }}</h2>
+    <h2>Last name: {{ lastName }}</h2>
+    <h3>{{ age }}</h3>
+    <h2>Full name: {{ uName }}</h2>
+    <h1>End component</h1>
+  </div>
 </template>
 
 <script>
 import { computed } from 'vue';
 export default {
-    props: ['firstName', 'lastName', 'age'],
-    setup(props) {
-        const uName = computed(function() {
-            return props.firstName + ' ' + props.lastName;
-        });
+  props: ['firstName', 'lastName', 'age'],
+  setup(props, context) {
+    const uName = computed(function () {
+      return props.firstName + ' ' + props.lastName;
+    });
 
-
-        return {
-            uName
-        }
-    }
-}
+    console.log(context);
+    return {
+      uName,
+    };
+  },
+};
 </script>
